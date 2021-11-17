7ed48e0ec5f61c530d760649b2271a55d5b346b6
Peter Herkenrath <p.herkenrath@uni-koeln.de>
2021-11-17 18:57:08 +0100

    efhir* added

---
+++ src/efhir_app.erl
@@ -0,0 +1,16 @@
+-module(efhir_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    efhir_sup:start_link().
+
+stop(_State) ->
+    ok.
